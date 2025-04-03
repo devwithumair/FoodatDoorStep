3d726d3dcdb8f973d118c9bf27c2d53be9ea737f
devwithumair <tumair139@gmail.com>
2025-04-03 21:31:47 +0500

    updated

--- lib/widgets/custom_bottomnavigationbar.dart
+++ lib/widgets/custom_bottomnavigationbar.dart
@@ -1,32 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:footatdoorstep/viewmodels/home_viewmodel.dart';
-import 'package:get/get.dart';
 
 class CustomBottomNavBar extends StatelessWidget {
   const CustomBottomNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final HomeViewModel controller = Get.find<HomeViewModel>();
-
-    return Obx(
-      () => BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.white,
-        selectedItemColor: Colors.purple,
-        unselectedItemColor: Colors.grey,
-        showUnselectedLabels: true,
-        currentIndex: controller.currentIndex.value,
-        onTap: controller.changeTabIndex,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: "Cart",
-          ),
-        ],
-      ),
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.white,
+      selectedItemColor: Colors.purple,
+      unselectedItemColor: Colors.grey,
+      showUnselectedLabels: true,
+      currentIndex: 0, // Add logic to manage index
+      onTap: (index) {
+        // Handle tab change
+      },
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Cart"),
+      ],
     );
   }
 }
