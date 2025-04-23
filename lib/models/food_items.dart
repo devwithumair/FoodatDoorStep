3813505aeeda4e3ed2de90892fd50e8b3f4ef34b
devwithumair <tumair139@gmail.com>
2025-04-23 21:49:53 +0500

    updated

---
+++ lib/models/food_items.dart
@@ -0,0 +1,17 @@
+class FoodItem {
+  final String id;
+  final String name;
+  final String image;
+  final double price;
+  final double rating;
+  final String description;
+
+  FoodItem({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.rating,
+    required this.description,
+  });
+}
